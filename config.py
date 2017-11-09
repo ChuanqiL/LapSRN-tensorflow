ffbb4bda01aa820a04368173cbdb2a8fa5f6b998
richardxiong <richard_xiong@DN0a1e4672.SUNet>
2017-11-09 02:10:51 -0800

    increase learning rate

--- config.py
+++ config.py
@@ -29,9 +29,9 @@ config.train.batch_size_each_folder = 30
 config.train.log_write = False
 
 ## Adam
-config.train.lr_init = 1e-5
-config.train.lr_decay = 0.95
-config.train.decay_iter = 10
+config.train.lr_init = 2e-5
+config.train.lr_decay = 0.90
+config.train.decay_iter = 15
 config.train.beta1 = 0.90
 
 ## initialize G
