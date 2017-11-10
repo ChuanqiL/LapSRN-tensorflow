65305b8d417e60e55bf72b2845c7b4f6371293ab
richardxiong <richard_xiong@DN51skvg.SUNet>
2017-11-09 18:24:20 -0800

    larger lr for binary

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ config.train.batch_size_each_folder = 30
 config.train.log_write = False
 
 ## Adam
-config.train.lr_init = 2e-5
+config.train.lr_init = 4e-5
 config.train.lr_decay = 0.90
 config.train.decay_iter = 15
 config.train.beta1 = 0.90
