664e4f248c64be65a04e94c68a14cf892e6ec3ec
richardxiong <richard_xiong@dhcp-visitor-218-61.slac.stanford.edu>
2017-11-14 11:18:35 -0800

    fine tune on larger 2x loss

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ config.train.batch_size_each_folder = 30
 config.train.log_write = False
 
 ## Adam
-config.train.lr_init = 1e-5 # 7e-5 
+config.train.lr_init = 3e-5 # 7e-5 
 config.train.lr_decay = 0.90
 config.train.decay_iter = 20
 config.train.beta1 = 0.90
